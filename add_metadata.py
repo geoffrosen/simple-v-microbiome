26ec6d09abc81c23d9b880ee91d0622b962c8227
Geoff Rosen <geoff.rosen@gmail.com>
2015-08-07 09:31:16 -0700

    Added authorship and documentation information to add_metadata.py

--- add_metadata.py
+++ add_metadata.py
@@ -1,18 +1,110 @@
 #!/usr/bin/env python
 
+__doc__ = ''' add_metadata.py: This will add metadata to a tsv file
+
+It will match the element in the top row of the tsv file
+with one in the leftmost column of the mapping file.
+It has options that will allow you to select whether to remove
+samples that are not listed in the mapping file (-r), to select
+which columns from the mapping file should make their way into
+the output (-c), and what values of those columns should be
+allowed (-a). It will list for you the excluded samples (-w).
+It can read from the stdin or use a specified filepath (-i).
+It can output to stdout or use a specified filepath (-o).
+
+Please direct questions to %s
+
+Sample:
+
+Say you have an otu table (otu_table.tsv) that looks like this:
+
+#OTU ID    Sample_1  Sample_2  Sample_3
+Bact_1       5          6        8
+
+And a mapping file (mapping_file.tsv) that looks like this:
+#SampleID  Meta_1    Meta_2
+Sample_1     1          2
+Sample_2     2          1
+
+If you run: 
+python add_metadata.py -i otu_table.tsv -m mapping_file.tsv
+	-o output.tsv
+
+This will be the output:
+#OTU ID    Sample_1  Sample_2  Sample_3
+Meta_1       1          2        NA
+Meta_2       2          1        NA
+Bact_1       5          6        8
+
+If you run:
+python add_metadata.py -i otu_table.tsv -m mapping_file.tsv
+	-o output.tsv -r
+
+This will be the output:
+#OTU ID    Sample_1  Sample_2
+Meta_1       1          2
+Meta_2       2          1
+Bact_1       5          6
+
+If you run: 
+python add_metadata.py -i otu_table.tsv -m mapping_file.tsv
+	-o output.tsv -c "Meta_1"
+
+This will be the output:
+#OTU ID    Sample_1  Sample_2  Sample_3
+Meta_1       1          2        NA
+Bact_1       5          6        8
+
+If you run:
+python add_metadata.py -i otu_table.tsv -m mapping_file.tsv
+	-o output.tsv -c "Meta_1" -a "1"
+
+This will be the output:
+#OTU ID    Sample_1
+Meta_1       1
+Bact_1       5
+
+If you run:
+python add_metadata.py -i otu_table.tsv -m mapping_file.tsv
+	-o output.tsv -c "Meta_1,Meta_2" -a "1,2;2"
+
+This will be the output:
+#OTU ID    Sample_1
+Meta_1       1
+Meta_2       2
+Bact_1       5
+''' % 'geoff.rosen@gmail.com'
+
+__author__ = "Geoff Rosen"
+__maintainer__ = "Geoff Rosen"
+__email__ = "geoff.rosen@gmail.com"
+__version__ = "0.1.0"
+
 import argparse, csv, sys
 
 def main():
-	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description=__doc__)
 	parser.add_argument('-i',help='tsv formatted otu table', default=None)
 	parser.add_argument('-m',help='mapping filepath. left-most column should be the same as the line headers of the tsv file.',required=True)
 	parser.add_argument('-o',help='output filepath', default=None)
 	parser.add_argument('-r',help='remove samples without metadata',action='store_true')
-	parser.add_argument('-c', help='categories to add (if not whole set, comma separated)',default=False)
+	parser.add_argument('-c', help='categories to add (if not whole set, comma separated)', default=False)
+	parser.add_argument('-a', help='allowed values in categories. (semi-colon separated between variables, comma separate between values)', default=False)
+	parser.add_argument('-w', help='write error(s)/removed sample(s) to stderr', action='store_true', default=False)
 	args = parser.parse_args()
 	opened_files = []
 	if args.c:
 		args.c = args.c.split(',')
+	if args.a:
+		args.r = True
+		args.a = args.a.split(';')
+		while len(args.a) != len(args.c):
+			args.a.append('')
+		args.a = dict(zip(args.c, args.a))
+		for cat, opts in args.a.iteritems():
+			args.a[cat] = opts.split(',')
+	if args.c:
+		args.c = set(args.c)
 	with open(args.m, 'rU') as mapping_file:
 		r = csv.reader(mapping_file, delimiter='\t')
 		header = r.next()[1:]
@@ -41,18 +133,24 @@ def main():
 			lineholder.append([item])
 			nheader.append(item)
 	headeritems = len(header)
-	errs = []
+	errs = set()
+	errs_samples = set()
 	for m in range(1,len(top)):
 		for i in range(len(nheader)):
 			if args.c and nheader[i] not in args.c:
 				continue
 			try:
-				lineholder[i].append(mapper[top[m]][nheader[i]])
+				if args.a and args.a[nheader[i]] != [''] and mapper[top[m]][nheader[i]] not in args.a[nheader[i]]:
+					lineholder[i].append('NA')
+					errs.add(m)
+					errs_samples.add(mapper[top[m]][header[0]])
+				else:
+					lineholder[i].append(mapper[top[m]][nheader[i]])
 			except:
 				lineholder[i].append('NA')
-				errs.append(m)
+				errs.add(m)
+				errs_samples.add(mapper[top[m]][header[0]])
 	if args.r:
-		errs = dict(zip(errs,errs))
 		w.writerow(get_rid_of_cols(top, errs))
 		for item in lineholder:
 			w.writerow(get_rid_of_cols(item, errs))
@@ -66,7 +164,13 @@ def main():
 			w.writerow(remrow)
 	for f in opened_files:
 		f.close()
-	sys.exit("finished with %s errors" % len(errs))
+	if args.w:
+		sys.stderr.write('No removed samples\n')
+		for samp in errs_samples:
+			sys.stderr.write(samp + '\n')
+		sys.exit("Finished with %s error(s)/removed sample(s)" % len(errs))
+	else:
+		sys.exit('Finished with %s error(s)/removed sample(s). If you would like to see them, use the "-w" flag.' % len(errs))
 	
 def get_rid_of_cols(row, bad_cols):
 	keep = []
