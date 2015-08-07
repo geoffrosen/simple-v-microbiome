3353b79d77ef5f819558751ce57e2996f7b5ab5d
Geoff Rosen <geoff.rosen@gmail.com>
2015-08-07 08:03:06 -0700

    Added stdout to add_metadata.py

--- add_metadata.py
+++ add_metadata.py
@@ -6,10 +6,11 @@ def main():
 	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('-i',help='tsv formatted otu table', default=None)
 	parser.add_argument('-m',help='mapping filepath. left-most column should be the same as the line headers of the tsv file.',required=True)
-	parser.add_argument('-o',help='output filepath',required=True)
+	parser.add_argument('-o',help='output filepath', default=None)
 	parser.add_argument('-r',help='remove samples without metadata',action='store_true')
 	parser.add_argument('-c', help='categories to add (if not whole set, comma separated)',default=False)
 	args = parser.parse_args()
+	opened_files = []
 	if args.c:
 		args.c = args.c.split(',')
 	with open(args.m, 'rU') as mapping_file:
@@ -22,45 +23,50 @@ def main():
 		input_file = sys.stdin
 	else:
 		input_file = open(args.i, 'rU')
-	with open(args.o, 'wb') as output_file:
-		r = csv.reader(input_file, delimiter='\t')
-		w = csv.writer(output_file, delimiter='\t')
-		top = r.next()
-		lineholder = []
-		nheader = []
-		for item in header:
-			if args.c and item not in args.c:
-				continue
-			else:
-				lineholder.append([item])
-				nheader.append(item)
-		headeritems = len(header)
-		errs = []
-		for m in range(1,len(top)):
-			for i in range(len(nheader)):
-				if args.c and nheader[i] not in args.c:
-					continue
-				try:
-					lineholder[i].append(mapper[top[m]][nheader[i]])
-				except:
-					lineholder[i].append('NA')
-					errs.append(m)
-		if args.r:
-			errs = dict(zip(errs,errs))
-			w.writerow(get_rid_of_cols(top, errs))
-			for item in lineholder:
-				w.writerow(get_rid_of_cols(item, errs))
-			for remrow in r:
-				w.writerow(get_rid_of_cols(remrow, errs))
+		opened_files.append(input_file)
+	if args.o == None:
+		output_file = sys.stdout
+	else:
+		output_file = open(args.o, 'wb')
+		opened_files.apend(output_file)
+	r = csv.reader(input_file, delimiter='\t')
+	w = csv.writer(output_file, delimiter='\t')
+	top = r.next()
+	lineholder = []
+	nheader = []
+	for item in header:
+		if args.c and item not in args.c:
+			continue
 		else:
-			w.writerow(top)
-			for item in lineholder:
-				w.writerow(item)
-			for remrow in r:
-				w.writerow(remrow)
-	if sys.stdin.isatty():
-		input_file.close()
-	print "finished with %s errors" % len(errs)
+			lineholder.append([item])
+			nheader.append(item)
+	headeritems = len(header)
+	errs = []
+	for m in range(1,len(top)):
+		for i in range(len(nheader)):
+			if args.c and nheader[i] not in args.c:
+				continue
+			try:
+				lineholder[i].append(mapper[top[m]][nheader[i]])
+			except:
+				lineholder[i].append('NA')
+				errs.append(m)
+	if args.r:
+		errs = dict(zip(errs,errs))
+		w.writerow(get_rid_of_cols(top, errs))
+		for item in lineholder:
+			w.writerow(get_rid_of_cols(item, errs))
+		for remrow in r:
+			w.writerow(get_rid_of_cols(remrow, errs))
+	else:
+		w.writerow(top)
+		for item in lineholder:
+			w.writerow(item)
+		for remrow in r:
+			w.writerow(remrow)
+	for f in opened_files:
+		f.close()
+	sys.exit("finished with %s errors" % len(errs))
 	
 def get_rid_of_cols(row, bad_cols):
 	keep = []
